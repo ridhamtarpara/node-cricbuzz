27e38e19a289304898685d43c58b669a31b350a1
Ridham Tarpara <ridham@zuru.tech>
2018-04-19 17:45:15 +0530

    feat: xml library changed to remove native c bindings
    - package.json updated

--- bin/recent_match.js
+++ bin/recent_match.js
@@ -1,36 +1,58 @@
 const request = require('request');
 const rp = require('request-promise');
-const parser = require('xml2json');
+const parseString = require('xml2js').parseString;
 
 const getRecentMatches = () => {
    return rp.get('http://synd.cricbuzz.com/j2me/1.0/livematches.xml')
-        .then(xml => {
-            const liveMatches = JSON.parse(parser.toJson(xml));
-            // console.log(JSON.stringify(liveMatches.mchdata, null, 2));
+        .then(xml => parseXML(xml))
+        .then(liveMatches => {
             const totalMatches = liveMatches.mchdata.match.length;
             const matchList = liveMatches.mchdata.match.reduce((result, match) => {
-                if (match.id > 15000) {
-                    result.push({
-                        id: match.id,
-                        type: match.type,
-                        series: match.srs,
-                        title: match.mchDesc,
-                        state: match.state.mchState,
-                        status: match.state.status,
+                if (match.$.id > 15000 || match.state[0].$.mchState === 'preview') {
+                    console.log({
+                        id: match.$.id,
+                        type: match.$.type,
+                        series: match.$.srs,
+                        title: match.$.mchDesc,
+                        state: match.state[0].$.mchState,
+                        status: match.state[0].$.status,
                         teams: match.Tm.map(team => ({
-                            name: team.Name || '',
-                            shortName: team.sName || ''
+                            name: team.$.Name || '',
+                            shortName: team.$.sName || ''
                         })),
-                        date: match.Tme.Dt,
-                        startTime: match.Tme.stTme,
-                        url: match.datapath
+                        date: match.Tme[0].$.Dt,
+                        startTime: match.Tme[0].$.stTme,
+                        url: match.$.datapath
+                    });
+                    result.push({
+                        id: match.$.id,
+                        type: match.$.type,
+                        series: match.$.srs,
+                        title: match.$.mchDesc,
+                        state: match.state[0].$.mchState,
+                        status: match.state[0].$.status,
+                        teams: match.Tm.map(team => ({
+                            name: team.$.Name || '',
+                            shortName: team.$.sName || ''
+                        })),
+                        date: match.Tme[0].$.Dt,
+                        startTime: match.Tme[0].$.stTme,
+                        url: match.$.datapath
                     });
                 }
                 return result;
-            }, [])
-            // console.log(JSON.stringify(matchList, null, 2));
+            }, []);
             return matchList;
-        })
+        });
+}
+
+const parseXML = (xml) => {
+    return new Promise((resolve, reject) => {
+        parseString(xml, (err, liveMatches) => {
+            if (err) reject(err);
+            else resolve(liveMatches);
+        }); 
+    });
 }
 
 module.exports = getRecentMatches;
